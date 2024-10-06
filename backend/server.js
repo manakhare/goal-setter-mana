2c38abfcbc231e8cc9ed7da5f6e007250d702a76
manakhare <manakhare5@gmail.com>
2024-10-06 19:33:43 +0530

    Deployment changes

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.get("/", (req, res) => {
+  res.send("Welcome to Goal Setter App!");
+})
+
 app.use("/api/goals", require("./routes/goalRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
 
